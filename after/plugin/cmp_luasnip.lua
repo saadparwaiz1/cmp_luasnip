3678f675e88f804dca3be5701c56199f72c03117
Saad Parwaiz <saad.parwaiz1@gmail.com>
2021-08-09 17:40:45 +0100

    LuaSnip Completion Source For nvim-cmp

---
+++ after/plugin/cmp_luasnip.lua
@@ -0,0 +1,1 @@
+require'cmp'.register_source('luasnip', require'cmp_luasnip'.new())
---
+++ lua/cmp_luasnip/init.lua
@@ -0,0 +1,55 @@
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local util = require('vim.lsp.util')
+
+local source = {}
+
+source.new = function()
+  return setmetatable({}, { __index = source })
+end
+
+function source:complete(request, callback)
+  local filetypes = { request.context.filetype, 'all' }
+  local items = {}
+
+  for i = 1, #filetypes do
+    local ft_table = luasnip.snippets[filetypes[i]]
+    if ft_table then
+      for j, snip in ipairs(ft_table) do
+        items[#items+1] = {
+          word = snip.trigger,
+          label = snip.trigger,
+          kind = cmp.lsp.CompletionItemKind.Snippet,
+          data = {
+            filetype = filetypes[i],
+            ft_indx = j
+          }
+        }
+      end
+    end
+  end
+  callback(items)
+end
+
+function source:resolve(completion_item, callback)
+  local item = completion_item
+  local snip = luasnip.snippets[item.data.filetype][item.data.ft_indx]
+  local header = (snip.name or "") .. " _ `[" .. completion_item.data.filetype .. ']`\n'
+  local documentation = { header .. string.rep('=', string.len(header) - 3), "", (snip.dscr or "")}
+  documentation = util.convert_input_to_markdown_lines(documentation)
+  documentation = table.concat(documentation, '\n')
+  completion_item.documentation = {
+    kind = cmp.lsp.MarkupKind.Markdown,
+    value =  documentation
+  }
+  callback(completion_item)
+end
+
+function source:execute(completion_item, callback)
+  local item = completion_item
+  local snip = luasnip.snippets[item.data.filetype][item.data.ft_indx]:copy()
+  snip:trigger_expand(Luasnip_current_nodes[vim.api.nvim_get_current_buf()])
+  callback(completion_item)
+end
+
+return source
