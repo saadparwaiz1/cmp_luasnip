71ebf1cc1c1e47ecb3819c36af7dcde670ee3e4c
Saad Parwaiz <saad.parwaiz1@gmail.com>
2021-09-02 21:10:45 +0100

    refactor

--- lua/cmp_luasnip/regex.lua
+++ lua/cmp_luasnip/regex.lua
@@ -1,111 +1,98 @@
-local M = {}
-local magic = {
-	["("] = true,
-	[")"] = true,
-	["."] = true,
-	["%"] = true,
-	["+"] = true,
-	["-"] = true,
-	["*"] = true,
-	["?"] = true,
-	["["] = true,
-	["]"] = true,
-	["^"] = true,
-	["$"] = true,
-	a = "a",
-	c = "\t",
-	d = "0",
-	l = "a",
-	p = ",",
-	s = " ",
-	u = "A",
-	w = "A",
-	x = "0",
-}
+	local magic = {
+		["("] = "(",
+		[")"] = ")",
+		["."] = ".",
+		["%"] = "%",
+		["+"] = "+",
+		["-"] = "-",
+		["*"] = "*",
+		["?"] = "?",
+		["["] = "[",
+		["]"] = "]",
+		["^"] = "^",
+		["$"] = "$",
+		a = "a",
+		c = "\t",
+		d = "0",
+		l = "a",
+		p = ",",
+		s = " ",
+		u = "A",
+		w = "A",
+		x = "0",
+	}
 
--- Create a Parser Object
---- @param trigger string
-M.new = function(trigger)
-	local obj = setmetatable({}, { __index = M })
-	obj.iterator = trigger:gmatch(".")
-	return obj
-end
-
-function M:handle_perct()
-	local cur_itm = self.iterator()
-	return magic[cur_itm] == true and cur_itm or magic[cur_itm]
-end
-
-function M:handle_char()
-	local remove_previous = nil
-	local cur_itm = self.iterator()
+local function handle_char(iterator)
+	local cur_itm = iterator()
 	local invert = cur_itm == "^"
 	if invert then
-		cur_itm = self.iterator()
+		cur_itm = iterator()
 	end
-	local cases = {}
+	local cases = {
+    ranges = {},
+    classes = {},
+    characters = {}
+  }
 
 	while cur_itm ~= "]" do
 		if cur_itm == "%" then
-			table.insert(cases, self:handle_perct())
+			table.insert(cases.classes, iterator())
 		elseif cur_itm == "-" then
-			if remove_previous then
-				cases[#cases] = nil
-			end
-			table.insert(cases, self.iterator())
-			remove_previous = false
+      local previous_item = cases.characters[#cases.characters]
+      cases.characters[#cases.characters] = nil
+			table.insert(cases.ranges, {previous_item, iterator()})
 		else
-			remove_previous = true
-			table.insert(cases, cur_itm)
+			table.insert(cases.characters, cur_itm)
 		end
-		cur_itm = self.iterator()
+		cur_itm = iterator()
 	end
-	-- TODO handle invert case
-	-- return the first case for non-invert case
+  if not invert then
+    if #cases.characters ~= 0 then
+      return cases.characters[1]
+    end
+
+    if #cases.classes ~= 0 then
+      return magic[cases.classes[1]]
+    end
+
+    if #cases.ranges ~= 0 then
+      return cases.ranges[1][1]
+    end
+  end
+  -- TODO Handle Invert Case
 	return cases
 end
 
 --- @param prefix string
 ---@param trigger string
 local function match_partially(prefix, trigger)
-	-- TODO consume as much of trigger as possible with prefix
 	local _, e = trigger:find(prefix)
 	return trigger:sub(e + 1)
 end
 
-local function construct_label(prefix, pattern)
-	-- TODO use prefix and pattern to construct a label
-	return prefix .. table.concat(pattern)
-end
-
--- construct labek for regex trigger
+-- construct label for regex trigger
 ---@param prefix  string
 ---@param trigger string
 local function construct(prefix, trigger)
 	if prefix:match(trigger) then
 		return prefix
 	end
-	local skip = { ["+"] = true, ["-"] = true, ["*"] = true, ["?"] = true, ["("] = true, [")"] = true }
-
 	trigger = match_partially(prefix, trigger)
-	print(trigger)
-	local parser = M.new(trigger)
-	local pattern = {}
-	for i in parser.iterator do
+	local iterator = trigger:gmatch(".")
+	for i in iterator do
 		if i == "%" then
-			table.insert(pattern, parser:handle_perct())
-		elseif skip[i] then
-			-- Skip
+      prefix = prefix .. magic[iterator()]
 		elseif i == "[" then
-			table.insert(pattern, parser:handle_char())
-		else
-			table.insert(pattern, i)
-		end
-	end
-	print(vim.inspect(pattern))
-	return construct_label(prefix, pattern)
+			prefix = prefix .. handle_char(iterator)
+    elseif i == '$' then
+      return prefix
+    elseif i == '^' then
+      prefix = ''
+		elseif i ~= '+' and i ~= '-' and i ~= '*' and i ~= "?" and i ~= "(" and i ~= ")" then
+      prefix = prefix .. i
+	  end
+  end
+	return prefix
 end
-
-print(construct("lor", "lorem(%d*)"))
 
 return construct
