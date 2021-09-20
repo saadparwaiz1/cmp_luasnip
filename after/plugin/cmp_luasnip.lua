a0fb34a0ecfd06ae74f90517bb4da1e27223ec34
Saad Parwaiz <saad.parwaiz1@gmail.com>
2021-09-20 18:49:19 +0100

    convert vimscript after plugin to lua

--- after/plugin/cmp_luasnip.lua
+++ after/plugin/cmp_luasnip.lua
@@ -1,1 +1,8 @@
 require'cmp'.register_source('luasnip', require'cmp_luasnip'.new())
+
+vim.api.nvim_exec([[
+  augroup cmp_luasnip
+    au!
+    autocmd User LuasnipCleanup lua require'cmp_luasnip'.clear_cache()
+  augroup END
+]], false)
--- after/plugin/cmp_luasnip.vim
+++
@@ -1,4 +0,0 @@
-augroup cmp_luasnip
-	au!
-	autocmd User LuasnipCleanup lua require'cmp_luasnip'.clear_cache()
-augroup END
